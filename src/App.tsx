3716052e35ec378179d61f0d485a859e719164be
chanchal013 <chanchal@compose.co.in>
2024-05-09 11:42:34 +0530

    feat: add speech to text functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "regenerator-runtime";
+import { useState, useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState<string>("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Speech recognition is not supported.</span>;
+  }
+
+  const requestMicrophonePermission = async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({ audio: true });
+    } catch (error) {
+      console.error(error);
+      alert("Error accessing microphone");
+    }
+  };
+
+  const toggleListening = () => {
+    if (!listening) {
+      requestMicrophonePermission().then(() => {
+        // resetTranscript();
+        SpeechRecognition.startListening();
+      });
+    } else {
+      SpeechRecognition.stopListening();
+    }
+  };
+
+  useEffect(() => {
+    if (transcript) {
+      // Update state with the recognized speech (transcript)
+      setQuery(transcript);
+    }
+  }, [transcript]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h2>Speech To Text</h2>
+      <div className="container">
+        <button onClick={toggleListening}>
+          {listening ? "listening" : "speak"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="transcript">
+          <p>
+            <strong>You said:</strong> {query}
+          </p>
+        </div>
+        <button
+          onClick={() => {
+            resetTranscript();
+            setQuery("");
+          }}
+        >
+          Clear Transcript
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
